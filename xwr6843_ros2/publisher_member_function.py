bf014425315efbc281c348173f5cc5c122409bed
nhma20 <76950970+nhma20@users.noreply.github.com>
2025-05-12 12:45:12 +0200

    option for doppler, snr, noise; optimization; bug fixes

--- xwr6843_ros2/publisher_member_function.py
+++ xwr6843_ros2/publisher_member_function.py
@@ -1,4 +1,3 @@
-
 import rclpy
 from rclpy.node import Node
 import time
@@ -14,8 +13,6 @@ import math
 
 MAGIC_WORD = b'\x02\x01\x04\x03\x06\x05\x08\x07'
 ms_per_frame = 9999.0
-global shut_down
-shut_down = 0
 global data_port
 data_port = '/dev/ttyUSB1'
 global cli_port
@@ -33,6 +30,12 @@ minimum_range = 0.25
 global cpu_cycles
 global frame_number
 global minimal_publisher
+global publish_velocity
+publish_velocity = True
+global publish_snr
+publish_snr = True
+global publish_noise
+publish_noise = True
 
 
 class TI:
@@ -157,9 +160,10 @@ class TI:
             Tuple [Tuple (int), int]
 
         """
-        magic, idx = self._unpack(byte_buffer, idx, order='>', items=1, form='Q')
-        (version, length, platform, frame_num, cpu_cycles, num_obj, num_tlvs), idx = self._unpack(byte_buffer, idx,
-                                                                                                    items=7, form='I')
+        # magic, idx = self._unpack(byte_buffer, idx, order='>', items=1, form='Q')
+        magic, idx = self._unpack(byte_buffer, idx, order='<', items=1, form='Q')
+        # (version, length, platform, frame_num, cpu_cycles, num_obj, num_tlvs), idx = self._unpack(byte_buffer, idx, items=7, form='I')
+        (version, length, platform, frame_num, cpu_cycles, num_obj, num_tlvs), idx = self._unpack(byte_buffer, idx, order='<', items=7, form='I')
         subframe_num, idx = self._unpack(byte_buffer, idx, items=1, form='I')
         return (version, length, platform, frame_num, cpu_cycles, num_obj, num_tlvs, subframe_num), idx
     
@@ -174,45 +178,56 @@ class TI:
         """ Parses the information of the detected points message
 
         """
-        (x,y,z,vel), idx = self._unpack(byte_buffer, idx, items=4, form='f')
+        # (x,y,z,vel), idx = self._unpack(byte_buffer, idx, items=4, form='f')
+        (x,y,z,vel), idx = self._unpack(byte_buffer, idx, order='<', items=4, form='f')
        
         return (x,y,z,vel), idx
+    
+    def _parse_msg_detected_points_side_info(self,byte_buffer, idx):
+        # (snr,noise), idx = self._unpack(byte_buffer, idx, items=2,form='H')
+        (snr,noise), idx = self._unpack(byte_buffer, idx, order='<', items=2, form='H')
+        return (snr,noise),idx
 
     def _process_detected_points(self, byte_buffer):
-            """
-            点云
-            """
-            if byte_buffer.find(MAGIC_WORD) == -1: # not present yet
-                fail_array = data=np.zeros((1,6),dtype=np.float)
-                return False, fail_array
-            idx = byte_buffer.index(MAGIC_WORD)
-            header_data, idx = self._parse_header_data(byte_buffer, idx)    
-            # print("Frame num: ", header_data[3], "CPU cycles: ", header_data[4])
-            # self.get_logger().info("Frame num: %s, CPU cycles %s" %  header_data[3], header_data[4])
-            global cpu_cycles
-            cpu_cycles = header_data[4]
-            global frame_number
-            frame_number = header_data[3]
+        if byte_buffer.find(MAGIC_WORD) == -1: # not present yet
+            fail_array = data=np.zeros((1,6),dtype=np.float)
+            return False, fail_array
+        idx = byte_buffer.index(MAGIC_WORD)
+        header_data, idx = self._parse_header_data(byte_buffer, idx)    
 
-  
-            num_tlvs=header_data[6]
-            
-            ####  tvl1  ####
+        global cpu_cycles
+        cpu_cycles = header_data[4]
+        global frame_number
+        frame_number = header_data[3]
+
+        num_tlvs=header_data[6]
+        num_points = header_data[5]
+        
+        data=np.zeros((num_points,6),dtype=np.float)
+
+        for _ in range(num_tlvs):
             (tlv_type, tlv_length), idx = self._parse_header_tlv(byte_buffer, idx)
-            num_points=int(tlv_length/16)
-            data=np.zeros((num_points,6),dtype=np.float)
-            for i in range(num_points):
-                ( x, y, z,vel), idx = self._parse_msg_detected_points(byte_buffer, idx)
-                data[i][0]=x
-                data[i][1]=y
-                data[i][2]=z
-                data[i][3]=vel
-                # data[i][4]=0
-                # data[i][5]=0
 
-            return True, data
+             ####  TVL1 - X Y Z Doppler  ####
+            if tlv_type == 1 and tlv_length == num_points * 4 * 4  and (idx + num_points * 4 * 4) < len(byte_buffer):
+                data[:, 0:4] = np.frombuffer(byte_buffer, dtype='<f4', count=num_points*4, offset=idx).reshape(num_points,4)
+                idx += tlv_length
+
+            ####  TLV7 -- SNR NOISE  ####
+            elif tlv_type == 7 and tlv_length == num_points * 4 and (idx + num_points * 4) < len(byte_buffer):
+                data[:, 4:6] = np.frombuffer(byte_buffer, dtype='<u2', count=num_points*2, offset=idx).reshape(num_points,2)
+                idx += tlv_length   # skip the entire block
+
+            # Other TLV → skip it
+            else:
+                idx += tlv_length
+
+        return True, data
+
+    
     @staticmethod
-    def _unpack(byte_buffer, idx, order='', items=1, form='I'):
+    # def _unpack(byte_buffer, idx, order='', items=1, form='I'):
+    def _unpack(byte_buffer, idx, order='<', items=1, form='I'):
         """Helper function for parsing binary byte data
 
         Args:
@@ -229,11 +244,16 @@ class TI:
         size = {'H': 2, 'h': 2, 'I': 4, 'Q': 8, 'f': 4}
         try:
             data = struct.unpack(order + str(items) + form, byte_buffer[idx:idx + (items * size[form])])
-            if len(data) == 1:
-                data = data[0]
+            # if len(data) == 1:
+                # data = data[0]
+            if items == 1:
+                data = (data[0],)   # always make it a tuple
             return data, idx + (items * size[form])
         except:
-            return None
+            # return None
+            # return a default tuple of the right length plus unchanged idx
+            default = tuple(0 for _ in range(items))
+            return default, idx
 
 
 class Detected_Points(Node):
@@ -249,6 +269,9 @@ class Detected_Points(Node):
         global radar_elevation_fov
         global radar_azimuth_fov
         global minimum_range
+        global publish_velocity
+        global publish_snr
+        global publish_noise
 
         self.declare_parameter('data_port', data_port)
         self.declare_parameter('cli_port', cli_port)
@@ -257,6 +280,9 @@ class Detected_Points(Node):
         self.declare_parameter('radar_elevation_fov', radar_elevation_fov)
         self.declare_parameter("radar_azimuth_fov", radar_azimuth_fov)
         self.declare_parameter("minimum_range", minimum_range)
+        self.declare_parameter("publish_velocity", publish_velocity)
+        self.declare_parameter("publish_snr", publish_snr)
+        self.declare_parameter("publish_noise", publish_noise)
 
         data_port = self.get_parameter('data_port').get_parameter_value().string_value
         cli_port = self.get_parameter('cli_port').get_parameter_value().string_value
@@ -265,9 +291,48 @@ class Detected_Points(Node):
         radar_elevation_fov = self.get_parameter('radar_elevation_fov').get_parameter_value().integer_value
         radar_azimuth_fov = self.get_parameter('radar_azimuth_fov').get_parameter_value().integer_value
         minimum_range = self.get_parameter('minimum_range').get_parameter_value().double_value
+        publish_velocity = self.get_parameter('publish_velocity').get_parameter_value().bool_value
+        publish_snr = self.get_parameter('publish_snr').get_parameter_value().bool_value
+        publish_noise = self.get_parameter('publish_noise').get_parameter_value().bool_value
 
         self.azimuth_tan_constant = math.tan( (radar_azimuth_fov*0.01745329) / 2 ) # 0.01745329 = rad per deg
         self.elevation_tan_constant = math.tan( (radar_elevation_fov*0.01745329) / 2)
+
+        self.fields = fields = [
+                    PointField(name='x', offset=0,  datatype=PointField.FLOAT32, count=1),
+                    PointField(name='y', offset=4,  datatype=PointField.FLOAT32, count=1),
+                    PointField(name='z', offset=8,  datatype=PointField.FLOAT32, count=1),
+
+                    # add vel if requested
+                    *(
+                    [ PointField(name='vel', offset=12, datatype=PointField.FLOAT32, count=1) ]
+                    if publish_velocity else []
+                    ),
+
+                    # add snr if requested (offset shifts by 4 if vel in or out)
+                    *(
+                    [ PointField(
+                        name='snr',
+                        offset=12 + (4 if publish_velocity else 0),
+                        datatype=PointField.FLOAT32,
+                        count=1
+                        )
+                    ] if publish_snr else []
+                    ),
+
+                    # add noise if requested (offset shifts by 4 for each prior inclusion)
+                    *(
+                    [ PointField(
+                        name='noise',
+                        offset=12
+                                    + (4 if publish_velocity   else 0)
+                                    + (4 if publish_snr   else 0),
+                        datatype=PointField.FLOAT32,
+                        count=1
+                        )
+                    ] if publish_noise else []
+                    ),
+                ]
 
         global ms_per_frame
         self.MAGIC_WORD = b'\x02\x01\x04\x03\x06\x05\x08\x07'
@@ -277,9 +342,9 @@ class Detected_Points(Node):
 
 
         self.publisher_ = self.create_publisher(PointCloud2, 'xwr6843_pcl', 10)
-        timer_period = float(ms_per_frame/2000) # poll new data 2x faster than frame rate
+        timer_period = float(ms_per_frame/5000) # poll new data 5x faster than frame rate
         self.timer = self.create_timer(timer_period, self.data_stream_iterator)
-
+        self.serial_data_wait_iterations =  ( 1.0 / timer_period ) * 5.0
         self.frame_number_array = [[],[],[],[],[],[],[],[],[],[]]
         self.frame_number_array_ptr = 0
         self.frame_number_array_len = len(self.frame_number_array)
@@ -293,92 +358,114 @@ class Detected_Points(Node):
         byte_buffer=self.ti._read_buffer()
         
         if(len(byte_buffer)==0):
-            self.warn+=1
+            self.warn += 1
         else:
-            self.warn=0
-        if(self.warn>100):#连续10次空读取则退出 / after 10 empty frames
-            print("Wrong")
-            return
+            self.warn = 0
+        if(self.warn > self.serial_data_wait_iterations): # after some seconds of empty buffer reads
+            global minimal_publisher
+            minimal_publisher.ti.close()
+            print("No serial data, radar ", frame_id, " exiting")
+            time.sleep(0.25)
+            exit(1)
     
         self.data+=byte_buffer
     
         try:
-            idx1 = self.data.index(MAGIC_WORD)   
-            idx2 = self.data.index(MAGIC_WORD,idx1+1)
+            idx = self.data.index(MAGIC_WORD)   
+            # idx1 = self.data.index(MAGIC_WORD)   
+            # idx2 = self.data.index(MAGIC_WORD,idx1+1)
 
         except:
+            return # No magic word found yet
+        
+        # Not enough data to read full header
+        if len(self.data) < idx + 40:
+            return
+        
+        # Peek at length from header (after magic word)
+        header_data, _ = self.ti._parse_header_data(self.data, idx)    
+
+        packet_len = header_data[1]
+
+        # Wait for full packet
+        if len(self.data) < idx + packet_len:
             return
 
-        self.data=self.data[idx2:]
-        success, points=self.ti._process_detected_points(byte_buffer)
+        frame = self.data[idx:idx+packet_len]
+        self.data=self.data[idx+packet_len:]  # drop parsed frame
+        success, points=self.ti._process_detected_points(frame) # self.data
         
         if not success: # not ready yet
             return
 
-        ret=points[:,:3]
+        x = points[:,0]
+        y = points[:,1]
+        z = points[:,2]
 
+        # check that point is above minimum range & inside expected FOV 
+        with np.errstate(divide='ignore', invalid='ignore'):  # <-- suppress divide‑by‑zero here
+            mask = (
+                (y > minimum_range) &
+                (np.abs(x)/y < self.azimuth_tan_constant) &
+                (np.abs(z)/y < self.elevation_tan_constant)
+            )
+        filtered = points[mask]        # shape (M,6)
 
+        # pick columns based on requested data (vel, snr, noise)
+        cols = [0,1,2]
+        if publish_velocity: cols.append(3)
+        if publish_snr:      cols.append(4)
+        if publish_noise:    cols.append(5)
 
-        global shut_down
+        cloud_arr = filtered[:, cols].astype(np.float32)  # shape (M, K)
 
-        if not ret == []: 
-            temp_cloud_arr = np.asarray(ret).astype(np.float32) # on form [[x,y,z],[x,y,z],[x,y,z]..]
-            cloud_arr = []
+        # # vel_idx = cols.index(3)
+        # snr_idx = cols.index(4)
+        # # noise_idx = cols.index(5)
+        # if any(x > 1000 for x in cloud_arr[:, snr_idx].astype(np.uint16)):
+        #     print(cloud_arr[:, snr_idx].astype(np.uint16))
 
-            #filter points based on expected sensor FOV
-            for i in range(temp_cloud_arr.shape[0]):
-                if float(temp_cloud_arr[i][1]) > float(minimum_range): # above min range
-                    if float(abs(temp_cloud_arr[i][0]))/float(temp_cloud_arr[i][1]) < float(self.azimuth_tan_constant): # inside azimuth fov
-                        if float(abs(temp_cloud_arr[i][2]))/float(temp_cloud_arr[i][1]) < float(self.elevation_tan_constant): # inside elevation fov
-                            cloud_arr.append([temp_cloud_arr[i][0],temp_cloud_arr[i][1],temp_cloud_arr[i][2]])
+        # build PointCloud2 message to publish        
+        pcl_msg = PointCloud2()
+        pcl_msg.header = std_msgs.msg.Header()
+        pcl_msg.header.stamp = self.get_clock().now().to_msg()
+        pcl_msg.header.frame_id = frame_id 
 
-            cloud_arr = np.asarray(cloud_arr).astype(np.float32)
-            
-            pcl_msg = PointCloud2()
-            pcl_msg.header = std_msgs.msg.Header()
-            pcl_msg.header.stamp = self.get_clock().now().to_msg()
-            pcl_msg.header.frame_id = frame_id 
+        if np.size(cloud_arr) < 1: 
+            pcl_msg.height = 0 # because unordered cloud
+            pcl_msg.width = 0 # number of points in cloud
+            pcl_msg.is_dense = False
+        else:
+            pcl_msg.height = 1 # because unordered cloud
+            pcl_msg.width = cloud_arr.shape[0] # number of points in cloud
+            # define interpretation of pointcloud message (offset is in bytes, float32 is 4 bytes)
+            pcl_msg.fields = self.fields
+            #cloud_msg.is_bigendian = False # assumption        
+            pcl_msg.point_step = cloud_arr.dtype.itemsize*cloud_arr.shape[1] #size of 1 point (float32 * dimensions (3 when xyz, 6 when x y z vel snr noise))
+            pcl_msg.row_step = pcl_msg.point_step*cloud_arr.shape[0] # only 1 row because unordered
+            pcl_msg.is_dense = True
+            cloud_arr = np.ascontiguousarray(cloud_arr) # ensure it's a single C‑contiguous buffer
+            pcl_msg.data = memoryview(cloud_arr).cast('B') #cloud_arr.tostring()
 
-            if np.size(cloud_arr) < 1: 
-                pcl_msg.height = 0 # because unordered cloud
-                pcl_msg.width = 0 # number of points in cloud
-                pcl_msg.is_dense = False
-            else:
-                pcl_msg.height = 1 # because unordered cloud
-                pcl_msg.width = cloud_arr.shape[0] # number of points in cloud
-                # define interpretation of pointcloud message (offset is in bytes, float32 is 4 bytes)
-                pcl_msg.fields =   [PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
-                                    PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
-                                    PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1)]
-                #cloud_msg.is_bigendian = False # assumption        
-                pcl_msg.point_step = cloud_arr.dtype.itemsize*cloud_arr.shape[1] #size of 1 point (float32 * dimensions (3 when xyz))
-                pcl_msg.row_step = pcl_msg.point_step*cloud_arr.shape[0] # only 1 row because unordered
-                pcl_msg.is_dense = True
-                pcl_msg.data = cloud_arr.tostring()
+        
+        global cpu_cycles
+        global frame_number
 
-            
-            global cpu_cycles
-            global frame_number
+        # check if data is stale by comparing sequential frame numbers
+        self.frame_number_array[self.frame_number_array_ptr] = frame_number
+        self.frame_number_array_ptr = (self.frame_number_array_ptr + 1) % self.frame_number_array_len
 
-            # self.get_logger().info('Frame: %s' % frame_number )
-            # self.get_logger().info('CPU cycle: %s' % cpu_cycles )
-            self.frame_number_array[self.frame_number_array_ptr] = frame_number
-            self.frame_number_array_ptr = (self.frame_number_array_ptr + 1) % self.frame_number_array_len
+        if all(sublist == self.frame_number_array[0] for sublist in self.frame_number_array):
+            self.get_logger().fatal('\033[91mReceiving stale data, exiting %s radar\033[0m' % frame_id)
+            self.ti.close() # useless?
+            exit(1) # exit ungracefully to force respawn through launch file
 
-            if all(sublist == self.frame_number_array[0] for sublist in self.frame_number_array):
-                self.get_logger().fatal('\033[91mReceiving stale data, exiting %s radar\033[0m' % frame_id)
-                self.ti.close() # useless?
-                shut_down = 1
-                exit(1) # exit ungracefully to force respawn through launch file
-
-            self.get_logger().info('Publishing %s points' % pcl_msg.width )
-            self.publisher_.publish(pcl_msg)
+        self.get_logger().info('Publishing %s points' % pcl_msg.width )
+        self.publisher_.publish(pcl_msg)
 
 
 
 def ctrlc_handler(signum, frame):
-    global shut_down
-    shut_down = 1
     global minimal_publisher
     minimal_publisher.ti.close()
     time.sleep(0.25)
